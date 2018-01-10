f892ff75c8598389c7d201bbe756f2e3f00bb2c1
Jake Luton <jake.luton92@gmail.com>
2018-01-10 14:56:52 -0800

    Fixed merge issue.

--- database/index.js
+++ database/index.js
@@ -1,12 +1,8 @@
 const { Client } = require('pg');
 
-<<<<<<< HEAD
-const client = new Client();
-=======
 const client = new Client({
   database: 'venmoo',
 });
->>>>>>> lint
 
 client.connect();
 
@@ -15,25 +11,9 @@ const getTransactionHistory = function (userName) {
   transactions.created_timestamp, transactions.resolved_timestamp, transactions.description
   FROM transactions, users
   WHERE (transactions.sender_id=users.id AND users.name='${userName}')
-<<<<<<< HEAD
-<<<<<<< HEAD
-  OR (transactions.receiver_id=users.id AND users.name='${userName}')`
-=======
-  OR (transactions.receiver_id=users.id AND users.name='${userName}')`;
->>>>>>> 55f77715039f4ff7051c01a8b80d7dc4885bfc6b
-
-  client.query(queryString)
-    .then((results) => {
-      console.log(results);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-=======
   OR (transactions.receiver_id=users.id AND users.name='${userName}')`;
 
   return client.query(queryString);
->>>>>>> lint
 };
 
 module.exports.getTransactionHistory = getTransactionHistory;
